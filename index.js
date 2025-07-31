5410f759f571a8bc150aee46f96b7b8d913bd51d
John Paul Patigas <johnpaulpatigas@gmail.com>
2025-07-31 18:51:28 +0800

    07/31/25 Activity 2

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
+// DUMMY DATA
 const data = {
   users: [
     { id: 1, name: "Carl", age: 22 },
@@ -15,6 +18,31 @@ const data = {
   },
 };
 
+// CREATE
+app.post("/users", (req, res) => {
+  const { name, age } = req.body;
+
+  if (!name || !age)
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+
+  const newId =
+    data.users.length > 0
+      ? Math.max(...data.users.map((p) => parseInt(p.id))) + 1
+      : "1";
+
+  const newUser = { id: newId, name, age };
+
+  data.users.push(newUser);
+
+  res.status(201).json({
+    message: "User created sucessfully",
+    newUser,
+  });
+});
+
+// READ
 app.get("/users", (req, res) => {
   console.log(req);
   res.json(data.users);
@@ -29,6 +57,48 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.status(404).json({ error: "User not found" });
   }
+});
+
+// UPDATE
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, age } = req.body;
+
+  const checkUser = data.users.find((profile) => profile.id === parseInt(id));
+
+  if (!checkUser) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  if (!name || !age) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  checkUser.name = name;
+  checkUser.age = age;
+
+  res.status(200).json({
+    message: "User updated sucessfully",
+    checkUser,
+  });
+});
+
+// DELETE
+app.delete("/users/:id", function (req, res) {
+  const { id } = req.params;
+  const index = data.users.findIndex((profile) => profile.id === parseInt(id));
+
+  if (index === -1)
+    return res.status(400).json({
+      message: "No user found",
+    });
+
+  const deleteUser = data.users.splice(index, 1)[0];
+
+  res.status(201).json({
+    message: "User deleted successfully",
+    deleteUser,
+  });
 });
 
 app.listen(PORT, () => {
